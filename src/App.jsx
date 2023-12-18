229293384f5222308be30787f32e759f612d298a
HIHIHAHA <149809452+Farras000@users.noreply.github.com>
2023-12-18 19:19:06 +0700

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,9 @@ import { Navb } from "./Component/Navb.jsx";
 import Detail from "./Page/Detail";
 import { AllPage } from "./Page/AllPage";
 import { GenrePage } from "./Page/GenrePage";
-import { Read } from "./Page/Read";
+import Read from "./Page/Read";
+import Genre from "./Page/Genre";
+
 import { Abus } from "./Page/Abus";
 import { Foot } from "./Component/Foot.jsx";
 
@@ -25,7 +27,7 @@ function App(){
       <Route path="/popular" Component={Home}   />
       <Route path="/genre" Component={GenrePage} />
       <Route path="/read/:endpoint" Component={Read} />
-
+      <Route path="/genre/:endpoint" Component={Genre} />
 
       <Route  path="/us" Component={Abus} />     
  
