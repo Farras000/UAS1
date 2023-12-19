516b84b46179bd31bb7313469a4ffbb009e746c4
HIHIHAHA <149809452+Farras000@users.noreply.github.com>
2023-12-19 23:15:29 +0700

    Add files via upload

--- src/Page/Detail.jsx
+++ src/Page/Detail.jsx
@@ -12,9 +12,9 @@ const Detail = () => {
     fetch(`${import.meta.env.VITE_API_URL}/detail/${endpoint}`)
       .then(response => response.json())
       .then(data => {
-        const manhwaData = data.manhwa && data.manhwa[0]; 
+        const manhwaData = data.manhwa && data.manhwa[0];
         setComicDetail(manhwaData || {});
-        setLoading(false); 
+        setLoading(false);
       })
       .catch(error => {
         console.error('Error fetching comic details:', error);
@@ -24,81 +24,82 @@ const Detail = () => {
   return (
     <div>
       {loading ? (
-          <div className="loading-container">
+        <div className="loading-container">
           <div className="lds-ring"><div></div><div></div><div></div><div></div></div>
-        </div>   ) : (
-    <div className="container mt-4">
-  
-    <Row>
-      <Col >
-        <Card style={{backgroundColor:'#374366', color:'#F1F1E6' }} >
-          <Row>
-            <Col md={4}>
-            <Card.Img
-  variant="top"
-  src={comicDetail.thumbnail}
-  alt={comicDetail.title}
-  className="img-fluid"
-  style={{
-    maxWidth: '85%', 
-    height: 'auto',   
-    aspectRatio: '3/4',
-    margin: '25px',
-    borderRadius: '4px'
-  }}
-/>
+        </div>) : (
+        <div className="container mt-4">
 
+          <Row>
+            <Col >
+              <Card style={{ backgroundColor: '#374366', color: '#F1F1E6' }} >
+                <Row>
+                  <Col md={4}>
+                    <Card.Img
+                      variant="top"
+                      src={comicDetail.thumbnail}
+                      alt={comicDetail.title}
+                      className="img-fluid"
+                      style={{
+                        maxWidth: '85%',
+                        height: 'auto',
+                        aspectRatio: '3/4',
+                        margin: '25px',
+                        borderRadius: '4px'
+                      }}
+                    />
+
+                  </Col>
+                  <Col md={8}>
+                    <Card.Body className='crd' >
+                      <Card.Title>{comicDetail.title}</Card.Title>
+                      <Card.Text>{comicDetail.synopsis}</Card.Text>
+                      <h2>Genres</h2>
+                      <ul>
+                        {comicDetail.genres &&
+                          comicDetail.genres.map(genre => (
+                            <li key={genre.name}>{genre.name}</li>
+                          ))}
+                      </ul>
+                    </Card.Body>
+                  </Col>
+                </Row>
+              </Card>
             </Col>
-            <Col md={8}>
-              <Card.Body className='crd' > 
-                <Card.Title>{comicDetail.title}</Card.Title>
-                <Card.Text>{comicDetail.synopsis}</Card.Text>
-                <h2>Genres</h2>
-                <ul>
-                  {comicDetail.genres &&
-                    comicDetail.genres.map(genre => (
-                      <li key={genre.name}>{genre.name}</li>
-                    ))}
-                </ul>
-              </Card.Body>
+
+          </Row>
+          <Row style={{ alignContent: 'center' }} className="mt-4">
+            <Col md={12} >
+              <h2 style={{ textAlign: 'center', marginBottom: '30px', color: '#F1F1E6' }}>Chapters</h2>
+              <Row>
+                {comicDetail.chapters &&
+                  comicDetail.chapters.map((chapter, index) => (
+                    <Col
+                      key={index}
+                      xs={6} sm={6} md={4} lg={3} xl={2.5}
+                    >
+                      <Card style={{ marginBottom: '25px', backgroundColor: '#374366', color: '#F1F1E6' }}>
+                        <Card.Body style={{ border: ' ', borderRadius: '2px' }}>
+                          <Card.Title>{chapter.title}</Card.Title>
+                          <Card.Title>{chapter.name}</Card.Title>
+                          <Card.Subtitle className="mb-2 ">{chapter.date}</Card.Subtitle>
+                          <Button
+                            href={`/read/${chapter.endpoint}`}
+                            style={{ margin: '10px 0', backgroundColor: '#3E8A86' }}
+                          >
+                            Read Chapter
+                          </Button>
+                        </Card.Body>
+                      </Card>
+                    </Col>
+                  ))}
+              </Row>
+
             </Col>
           </Row>
-        </Card>
-      </Col>
-     
-    </Row>
-    <Row style={{ alignContent: 'center' }} className="mt-4">
-  <Col md={12} >
-    <h2 style={{ textAlign: 'center', marginBottom: '30px', color: '#F1F1E6'}}>Chapters</h2>
-    <Row>
-  {comicDetail.chapters &&
-    comicDetail.chapters.map((chapter, index) => (
-      <Col
-        key={index}
-        xs={6} sm={6} md={4} lg={3} xl={2.5} 
-      >
-        <Card style={{ marginBottom: '25px', backgroundColor: '#374366', color: '#F1F1E6' }}>
-          <Card.Body style={{ border: ' ', borderRadius: '2px' }}>
-            <Card.Title>{chapter.title}</Card.Title>
-            <Card.Subtitle className="mb-2 ">{chapter.date}</Card.Subtitle>
-            <Button
-              href={`/read/${chapter.endpoint}`}
-              style={{ margin: '10px 0', backgroundColor: '#3E8A86' }}
-            >
-              Read Chapter
-            </Button>
-          </Card.Body>
-        </Card>
-      </Col>
-    ))}
-</Row>
 
-  </Col>
-</Row>
 
- 
-  </div>)}
-  </div>
+        </div>)}
+    </div>
   );
 };
 
